02771b34be28199eed3f10a5b2606a40fb48cfc0
MinerHai <haiminer45@gmail.com>
2025-11-28 23:52:37 +0700

    Fix cloudinary

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -1,9 +1,19 @@
 import { v2 as cloudinary } from "cloudinary";
 
+const cloudName = process.env.CLOUDINARY_NAME;
+const apiKey = process.env.CLOUDINARY_API_KEY;
+const apiSecret = process.env.CLOUDINARY_API_SECRET;
+
+if (!cloudName || !apiKey || !apiSecret) {
+  console.error("❌ Missing Cloudinary environment variables");
+  process.exit(1);
+}
+
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME!,
-  api_key: process.env.CLOUDINARY_API_KEY!,
-  api_secret: process.env.CLOUDINARY_API_SECRET!,
-  secure: true,
+  cloud_name: cloudName,
+  api_key: apiKey,
+  api_secret: apiSecret,
+  secure: true, // recommended in prod
 });
+
 export default cloudinary;
